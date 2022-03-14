f72b1726a8f8ea4375457ed13a2124868a5144e7
Ed Page <eopage@gmail.com>
2022-03-14 13:19:03 -0500

    fix!: Remove all extra git code

--- crates/git-branch-stash/src/git/mod.rs
+++
@@ -1,7 +0,0 @@
-mod branches;
-mod protect;
-mod repo;
-
-pub use branches::*;
-pub use protect::*;
-pub use repo::*;
--- crates/git-branch-stash/src/lib.rs
+++ crates/git-branch-stash/src/lib.rs
@@ -1,10 +1,11 @@
 #![allow(clippy::collapsible_else_if)]
 
 pub mod config;
-pub mod git;
 
+pub use git::*;
 pub use snapshot::*;
 pub use stack::*;
 
+mod git;
 mod snapshot;
 mod stack;
